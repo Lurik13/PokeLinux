fb839a0970555839244b60d22bd8e5215bb5e852
Lurik13 <lucasribette.pro@gmail.com>
2025-04-06 12:03:00 +0200

    Formes
    Méga évolutions, Gigamax, formes régionales, formes de biomes, etc

---
+++ pokemon/forms.py
@@ -0,0 +1,30 @@
+from weaknesses import *
+from utils import *
+from data import SPRITE_URL
+
+def get_sprite(form_data):
+    form_url = form_data["pokemon"]["url"]
+    form_id = form_url.strip("/").split("/")[-1]
+    return SPRITE_URL.format(form_id)
+
+def get_forms(species):
+    forms = []
+    for form in species['varieties']:
+        if form['is_default'] is False:
+            form_dict = {
+                'form_french_name': "",
+                'sprite': "",
+                'types': None,
+                'weaknesses': None
+            }
+            form_data = get_data('pokemon-form', form['pokemon']['name'])
+
+            if form['pokemon']['name'].endswith('gmax') == False \
+             and form['pokemon']['name'].startswith('pikachu') == False:
+                types = [t["type"]["name"] for t in form_data["types"]]
+                form_dict['types'] = [TYPE_TRANSLATION[t] for t in types]
+                form_dict['weaknesses'] = get_weaknesses(types)
+            form_dict['form_french_name'] = get_french_name(form_data)
+            form_dict['sprite'] = get_sprite(form_data)
+            forms.append(form_dict)
+    return forms
--- pokemon/get_pokemon.py
+++ pokemon/get_pokemon.py
@@ -1,28 +1,35 @@
-from evolution_chain import *
 from utils import *
 from weaknesses import *
+from forms import *
 
 def get_pokemon(pokemon_id):
     pokemon = get_data('pokemon', pokemon_id)
     species = get_data('pokemon-species', pokemon_id)
-    
-    evolutions = get_evolution_chain(species)
     number = pokemon["id"]
     french_name = get_french_name(species)
     types = [t["type"]["name"] for t in pokemon["types"]]
     weaknesses = get_weaknesses(types)
 
     return {
-        "evolutions": evolutions if french_name == evolutions[0] else None,
         "number": number,
         "english_name": pokemon["name"].capitalize(),
         "french_name": french_name,
         "sprite": SPRITE_URL.format(number),
         "types": [TYPE_TRANSLATION[t] for t in types],
-        "weaknesses": list(weaknesses)
+        "weaknesses": list(weaknesses),
+        "forms": get_forms(species),
     }
 
-pokemon = get_pokemon(103)
+pokemon = get_pokemon(6)
 for key, value in pokemon.items():
-    print(key, ":", value)
-    
+    if key == 'forms':
+        if value:
+            print("forms :")
+            for form in value:
+                print("   ", form['form_french_name'])
+                print("      ", form['sprite'])
+                if form['types'] is not None:
+                    print("      ", form['types'])
+                    print("      ", form['weaknesses'])
+    else:
+        print(key, ":", value)
