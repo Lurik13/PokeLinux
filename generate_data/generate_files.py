9a36e3531722149bb7d44b0e6e7ac9d99e59999b
Lurik13 <lucasribette.pro@gmail.com>
2025-04-08 21:32:55 +0200

    Génération automatique du fichier de relations

--- generate_data/generate_files.py
+++ generate_data/generate_files.py
@@ -5,29 +5,30 @@ from genericpath import exists
 sys.path.append(os.path.abspath('..'))
 from data.Knowledge.generations import *
 
-def get_generation_number(pokemon_id):
+def get_generation_number(pokemon_id): ###
 	for i in range(len(GENERATIONS)):
 		if pokemon_id >= GENERATIONS[i + 1]['pokemon_range'][0] and pokemon_id <= GENERATIONS[i + 1]['pokemon_range'][1]:
 			return i + 1
 	return 0
 
-def get_gen_region(gen_name):
+def get_gen_region(gen_name): ###
 	words = gen_name.strip().split()
 	if not words:
 		return None
 	return words[-1]
 
-def generate_folder(gen_number):
-	folder_name = get_gen_region(GENERATIONS[gen_number]['name'])
-	folder_path = "../data/" + folder_name
+def generate_folder(path, suffix):
+	folder_path = path + suffix
+	if not exists(path):
+		os.mkdir(path)
 	if not exists(folder_path):
 		os.mkdir(folder_path)
-		print("Generated the " + folder_name + " folder.")
+		print("Generated the " + suffix + " folder.")
 	return folder_path
 
 HEHE = {
-	"number": 1,
-	"french_name": "Test",
+	"number": 1024,
+	"french_name": "Mouahaha",
 	"english_name": "Attempt",
 	"evolution_chain": ['Essai', 'Test', 'Tentative'],
 	"sprite": "test.gif",
@@ -68,7 +69,6 @@ def write_file(file, pokemon):
 			case _:
 				new_text += letter
 	file.writelines(new_text)
-	file.close()
 	
 
 def generate_file(folder_name, pokemon):
@@ -76,12 +76,26 @@ def generate_file(folder_name, pokemon):
 	file_path = folder_name + '/' + file_name
 	file = open(file_path, 'w')
 	write_file(file, pokemon)
+	file.close()
 	print("Generated the " + file_name + " file.")
 
+def generate_pokemon_relations_file():
+	file = open("../data/Pokemons/pokemon_relations.py", 'w')
+	poke_imports = ""
+	poke_dict = ""
+	gen_number = get_generation_number(900)
+	poke_dict += "\nPOKEMON = {\n"
+	poke_imports += "from data.Pokemons." + get_gen_region(GENERATIONS[gen_number]['name']) \
+		+ "." + HEHE['french_name'] + " import " + HEHE['french_name'].upper() + "\n"
+	poke_dict += "    " + str(HEHE['number']) + ": " + HEHE['french_name'].upper() + "\n"
+	poke_dict += "}"
+	file.writelines(poke_imports + poke_dict)
+	file.close()
 
 gen_number = get_generation_number(900)
-folder_path = generate_folder(gen_number)
+folder_path = generate_folder("../data/Pokemons/", get_gen_region(GENERATIONS[gen_number]['name']))
 generate_file(folder_path, HEHE)
+generate_pokemon_relations_file()
 
 # print(POKEMON[3]['name'])
 
